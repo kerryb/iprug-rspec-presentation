18a4d74eea4f07dc2ab43e9dd76fcde89deb5c2d
Kerry Buckley <kerryjbuckley@gmail.com>
2011-01-01 21:06:25 +0000

    Set ruby snippet classes in javascript

---
+++ kerryb.js
@@ -0,0 +1,7 @@
+/*jslint white: true, evil: true,browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global window: false, jQuery: false */
+"use strict";
+
+$(document).ready(function(){
+  $(".ruby").addClass("brush: ruby; gutter: false; toolbar: false");
+});
